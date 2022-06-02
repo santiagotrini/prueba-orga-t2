aee63429f05e1112a23b3bcaa475c6da8c0fbc51
Santiago Trini <santiagotrini@gmail.com>
2022-06-01 21:55:28 -0300

    agrega atoi

--- iguales.s
+++ iguales.s
@@ -2,3 +2,36 @@
 # ejemplo
 # spim -q -f iguales.s 1 1 1 -> iguales
 # spim -q -f iguales.s 2 1 1 -> no iguales
+
+
+# funcion atoi no borrar
+atoi:
+  addi $sp, $sp, -8
+  sw $s0, 0($sp)
+  sw $s1, 4($sp)
+  sw $s2, 8($sp)
+  sw $s3, 12($sp)
+  li $v0, 0
+  li $s2, 1
+  li $s3, 45
+atoi_loop:
+  lb $s0, 0($a0)
+  beq $s0, $zero, ret
+  beq $s0, $s3, atoi_negative
+  mul $v0, $v0, 10
+  addi $s1, $s0, -48
+  add $v0, $v0, $s1
+atoi_inc:
+  addi $a0, $a0, 1
+  j atoi_loop
+ret:
+  mul $v0, $v0, $s2
+  lw $s0, 0($sp)
+  lw $s1, 4($sp)
+  lw $s2, 8($sp)
+  lw $s3, 12($sp)
+  addi $sp, $sp, 8
+  jr $ra
+atoi_negative:
+  li $s2, -1
+  j atoi_inc
